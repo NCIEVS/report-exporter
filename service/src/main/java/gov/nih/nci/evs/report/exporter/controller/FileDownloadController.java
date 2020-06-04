56957e41f6eef0bf59b0b454de573e72213c3038
hsbauer <bauer.scott@mayo.edu>
2020-06-04 16:15:11 -0500

    EVSREPEXP-5.  Adding a property id and file name creation capability to the download.

--- service/src/main/java/gov/nih/nci/evs/report/exporter/controller/FileDownloadController.java
+++ service/src/main/java/gov/nih/nci/evs/report/exporter/controller/FileDownloadController.java
@@ -3,6 +3,9 @@ package gov.nih.nci.evs.report.exporter.controller;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +25,9 @@ import gov.nih.nci.evs.report.exporter.service.CodeReadService;
 @RequestMapping("/download")
 public class FileDownloadController {
 	
+	public enum formats{JSON,CSV,TABD, EXCEL};
+	
+	
 	@Autowired
 	CodeReadService service;
 	
@@ -39,10 +45,12 @@ public class FileDownloadController {
 			}
 	
 	@GetMapping(
-			  value = "/get-file-for-props/{id}/{props}/JsonFile.json",
+			  value = "/get-file-for-props/{id}/{props}/{filename}",
 			  produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
 			)
-			public @ResponseBody byte[] getFileForProps(@PathVariable String id, @PathVariable String props) throws IOException {
+			public @ResponseBody byte[] getFileForProps(
+					@PathVariable String id, 
+					@PathVariable String props) throws IOException {
 			    InputStream in = new ByteArrayInputStream(service.getGsonForPrettyPrint().toJson(
 			    		service.getEntitiesForPropertyNameFilter(service.getRestProperties(
 			    				service.getRestTemplate(
@@ -50,5 +58,12 @@ public class FileDownloadController {
 			    				service.getCodes(id)), service.getCodes(props))).getBytes());
 			    return IOUtils.toByteArray(in);
 			}
+	
+	
+		@GetMapping("/output-formats")
+		public List<String> getFormatOutput(){
+			return Stream.of(formats.values()).map(
+					x -> x.name()).collect(Collectors.toList());
+		}
 
 }
