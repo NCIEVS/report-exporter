a5d2e7ca959a6c07850fd3b50e2c8bd9a84eac5b
Cory Endle <cory.endle@nih.gov>
2020-06-05 13:46:10 -0500

    EVSREPEXP-15 - added api file to call service

---
+++ clientui/src/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const api = {
+    
+    
+	getCodes(baseUrl, codes){
+        return new Promise((resolve)=>{
+            axios.get(baseUrl + '/codereadrest/' + codes)
+            .then((response) =>{
+            resolve(response.data);
+            })   
+        })
+    },
+    
+    getProperties(baseUrl){
+        return new Promise((resolve)=>{
+            axios.get(baseUrl + '/properties')
+            .then((response) =>{
+            resolve(response.data);
+            })   
+        })
+    },
+    
+}
+export default api
