0c4393e099d8db58742722652edf7e5888b9f0ea
hsbauer <bauer.scott@mayo.edu>
2020-06-18 15:41:26 -0500

    EVSREPEXP-24. Adding the export page user entered node code. Full slice implementation to the JSON download

--- service/src/main/java/gov/nih/nci/evs/report/exporter/controller/BranchResolutionController.java
+++ service/src/main/java/gov/nih/nci/evs/report/exporter/controller/BranchResolutionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import gov.nih.nci.evs.report.exporter.service.CodeReadService;
 import gov.nih.nci.evs.report.exporter.util.CommonServices;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
 public class BranchResolutionController {
 
 	@Autowired
