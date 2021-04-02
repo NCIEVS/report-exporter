13a57f01d5ea7bb7a104759c77c9545ccdf1ecb6
Scott Bauer <bauer.scott@mayo.edu>
2021-04-02 16:41:21 -0500

    EVSREPEXP-360.  Controller tests

---
+++ service/src/test/java/gov/nih/nci/evs/report/exporter/controller/RoleControllerTest.java
@@ -0,0 +1,19 @@
+package gov.nih.nci.evs.report.exporter.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleControllerTest {
+	@Autowired
+	RoleController controller;
+
+	@Test
+	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+}
