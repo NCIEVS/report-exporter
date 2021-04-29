1778e884b56ceca7192d76d616f8b592201df015
Scott Bauer <bauer.scott@mayo.edu>
2021-04-29 09:30:30 -0500

    EVSREPEXP-403. Code cleanup

--- service/src/main/java/gov/nih/nci/evs/report/exporter/service/EVSAPIBaseService.java
+++ service/src/main/java/gov/nih/nci/evs/report/exporter/service/EVSAPIBaseService.java
@@ -9,7 +9,6 @@ import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -27,9 +26,6 @@ import gov.nih.nci.evs.report.exporter.util.CommonServices;
 @Service
 public class EVSAPIBaseService {
 	
-//	@Autowired
-//	RoleService roleService;
-//	
 	private Logger log = LoggerFactory.getLogger(TimedDeferredResultWrapper.class);
 	
     @Value("${NODE_LIST}")
