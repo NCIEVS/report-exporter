89bae958bccbb9f17477c951cfcd732563f5a7c6
cendle <endle.cory@mayo.edu>
2021-08-03 11:29:08 -0500

    added more formatting

--- clientui/src/components/About.vue
+++ clientui/src/components/About.vue
@@ -3,7 +3,6 @@
      When you have a great story about how your product or service was built to change lives, share it. The "About Us" page is a great place for it to live, too. Good stories humanize your brand, providing context and meaning for your product. What’s more, good stories are sticky -- which means people are more likely to connect with them and pass them on.
   </div>
 </template>
-
 <script>
 export default {
   name: 'about'
