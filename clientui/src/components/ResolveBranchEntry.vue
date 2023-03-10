9f3a4f9590429fb25fbacc3b7ce0e1f15ff1da0a
amoore2020 <110399572+amoore2020@users.noreply.github.com>
2023-03-10 02:49:09 -0500

    EVSREPEXP-446 Vue2 to Vue3 update

--- clientui/src/components/ResolveBranchEntry.vue
+++ clientui/src/components/ResolveBranchEntry.vue
@@ -140,7 +140,7 @@
                   <input placeholder="Search properties" class="msl-search-list-input custom-input-class" id = "searchProperties" @keyup = "searchPropertiesFilter()">
                   <select multiple v-model="leftSelectedUsers" @dblclick="moveRight" class="msl-searchable-list__items" id = "selectSearchProperties">
                     <option v-for="userLeft in leftUsers" :key="userLeft" class="multi-select-option msl-searchable-list__item" id = "optionSearchProperties">
-                      {{ userLeft }}
+                      {{ userLeft.name }}
                     </option>
                   </select>
                 </div>
