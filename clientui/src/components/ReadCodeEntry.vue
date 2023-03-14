5a4ea024766ba1ca6d5a1e6e343b2d3187cbc29b
amoore2020 <110399572+amoore2020@users.noreply.github.com>
2023-03-14 15:39:29 -0400

    EVSREPEXP-446 Vue2 to Vue3 update

--- clientui/src/components/ReadCodeEntry.vue
+++ clientui/src/components/ReadCodeEntry.vue
@@ -365,7 +365,7 @@ export default {
               if ((data !== null) && (data!== undefined) && (data!== "")) {
                 for (let x = data.length - 1; x >= 0; x--) {
                   alert(data[x].name);
-                  if ((data[x].name != null) && (data[x].name!== undefined)) {
+                  if ((data[x].name != null) && (data[x].name!== undefined)  && (data[x].name !== "") && (data[x].length < 1)) {
                     //  if ((data[x].name.length > 0) && (data[x].name!== undefined)) {
 
                     //  alert("Code: " + data[x].code + " is invalid: " + data[x].queryStatus + " roles: " + data[x].roles + " association: " + data[x].associations + " Description: " + data[x].name);
