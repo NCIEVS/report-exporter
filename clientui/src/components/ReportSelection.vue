7fb5d860c028181c9c8b8602eabdf5c4ec280b2a
amoore2020 <110399572+amoore2020@users.noreply.github.com>
2023-04-05 05:15:22 -0400

    EVSREPEXP-446 Vue2 to Vue3 update

--- clientui/src/components/ReportSelection.vue
+++ clientui/src/components/ReportSelection.vue
@@ -121,6 +121,38 @@ export default {
   metaInfo: {
     title: 'EVS Report Exporter - Report Selection',
   },
+  mounted(){
+
+    this.$notify({
+      group: 'download',
+      title: 'Warning:',
+      text: 'You are accessing a US Government web site which may contain information that must be protected under the US Privacy Act or other sensitive information and is intended for Government authorized use only Unauthorized attempts to upload information, change information, or use of this web site may result in disciplinary action, civil, and/or criminal penalties. Unauthorized users of this website should have no expectation of privacy regarding any communications or data processed by this website. Anyone accessing this website expressly consents to monitoring of their actions and all communications or data transiting or stored on related to this website and is advised that if such monitoring reveals possible evidence of criminal activity, NIH may provide that evidence to law enforcement officials.',
+      type: 'success',
+      duration: 10000,
+      position: "Center"
+    });
+
+    /*
+    alert ("       You are accessing a US Government web site which may\n" +
+        "          contain information that must be protected under the US Privacy Act\n" +
+        "          or other sensitive information and is intended for Government\n" +
+        "          authorized use only\n" +
+        "           Unauthorized attempts to upload information, change information,\n" +
+        "          or use of this web site may result in disciplinary action, civil,\n" +
+        "          and/or criminal penalties. Unauthorized users of this website\n" +
+        "          should have no expectation of privacy regarding any communications\n" +
+        "          or data processed by this website.\n" +
+        "          Anyone accessing this website expressly consents to monitoring of\n" +
+        "          their actions and all communications or data transiting or stored\n" +
+        "          on related to this website and is advised that if such monitoring\n" +
+        "          reveals possible evidence of criminal activity, NIH may provide\n" +
+        "          that evidence to law enforcement officials.")
+
+     */
+  },
+
+
+
   data(){
     return {
       isModalVisible: false,
@@ -132,9 +164,22 @@ export default {
   methods: {
     showModal() {
       var warningCookie = this.$cookies.get(this.warningCookie);
-      alert(warningCookie);
+     // alert(warningCookie);
       if (!warningCookie) {
-        this.isModalVisible = true;
+        alert ("       You are accessing a US Government web site which may\n" +
+            "          contain information that must be protected under the US Privacy Act\n" +
+            "          or other sensitive information and is intended for Government\n" +
+            "          authorized use only\n" +
+            "           Unauthorized attempts to upload information, change information,\n" +
+            "          or use of this web site may result in disciplinary action, civil,\n" +
+            "          and/or criminal penalties. Unauthorized users of this website\n" +
+            "          should have no expectation of privacy regarding any communications\n" +
+            "          or data processed by this website.\n" +
+            "          Anyone accessing this website expressly consents to monitoring of\n" +
+            "          their actions and all communications or data transiting or stored\n" +
+            "          on related to this website and is advised that if such monitoring\n" +
+            "          reveals possible evidence of criminal activity, NIH may provide\n" +
+            "          that evidence to law enforcement officials.")
       }
     },
 
