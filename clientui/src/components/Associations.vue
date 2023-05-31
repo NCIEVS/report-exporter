aa9d5d215d6c0724e1ddd483c5bfa8635fb10e4e
amoore2020 <110399572+amoore2020@users.noreply.github.com>
2023-05-31 13:33:22 -0400

    EVSREPEXP-483 Associations Retired Message

--- clientui/src/components/Associations.vue
+++ clientui/src/components/Associations.vue
@@ -339,6 +339,7 @@ export default {
       var dupTagCheck = false;  // Vue 3 temporary variable used to make sure duplicate blue tags are not created
       tag = tag.replace(/[\s/]/g, '')
       tag = tag.replace(',', '')  // Vue 3 removes commas if entered in the text box
+      var tempStatus = ''
 
       this.setSelectedTags()  // Vue 3 this method takes the code description combo ex. (C12219:Anatomic Structure System or Substance) and returns only the code ex (C12219)
 
@@ -367,15 +368,16 @@ export default {
                       this.newTagCounter = this.newTagCounter + 1;
                     }
                   }else{
+                    tempStatus = data[x].queryStatus
                     //    this.tags.push(tag + ":" + "");   //Vue 3 used for testing take out after testing
                     //    this.newTag = ""                  //Vue 3 used for testing take out after testing
                     //     this.tagCounter = this.tagCounter + 1;  //Vue 3 used for testing take out after testing
                     this.$notify({
                       group: 'app',
-                      title: 'Validation Failure',
-                      text: 'Could not verify concept code(s).  Possible network issue.',
+                      title: 'Invalid Concept Code',
+                      text: '<b>' +tag+'</b> is not valid. <br>Reason: ' +tempStatus+ '.',
                       type: 'error',
-                      duration: 4000,
+                      duration: 6000,
                       position: "left bottom"
                     });
                   }
@@ -390,10 +392,10 @@ export default {
                   //       this.tagCounter = this.tagCounter + 1;  //Vue 3 used for testing take out after testing
                   this.$notify({
                     group: 'app',
-                    title: 'Validation Failure',
-                    text: 'Could not verify concept code(s).  Possible network issue.',
+                    title: 'Invalid Concept Code',
+                    text: '<b>' +tag+'</b> is not valid. <br>Reason: ' +tempStatus+ '.',
                     type: 'error',
-                    duration: 4000,
+                    duration: 6000,
                     position: "left bottom"
                   });
                 }
