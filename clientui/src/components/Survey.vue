fe10a12996d4b79aa76a5bd0daa3db6d2153985e
amoore2020 <110399572+amoore2020@users.noreply.github.com>
2023-07-14 17:16:26 -0400

    EVSREPEXP-478 Feedback form is not coming up

--- clientui/src/components/Survey.vue
+++ clientui/src/components/Survey.vue
@@ -2,6 +2,8 @@
   import axios from 'axios';
   import VueFormGenerator from 'vue-form-generator'
    //import 'vue-form-generator/dist/vfg.css'
+  //vue 3 counter for (Next) button due to form-wizard not working
+  let selectNextOptionBTN_counter =  1;
 
   export default {
     name: 'modal',
@@ -70,7 +72,96 @@
        }
      }
    },
+
+    // Vue 3 Method used for hiding options on first Survey Screen
+    mounted() {
+      document.getElementById("surveyBackButton").style.display = "none";
+      document.getElementById("surveyQuestion1").style.display = "none";
+      document.getElementById("surveyAnswer1").style.display = "none";
+      document.getElementById("surveyQuestion2").style.display = "none";
+      document.getElementById("surveyAnswer2").style.display = "none";
+      document.getElementById("surveyMaxCharacters").style.display = "none"
+      document.getElementById("surveyQuestion3").style.display = "none";
+      document.getElementById("surveyAnswer3").style.display = "none";
+      document.getElementById("surveyQuestion4").style.display = "none";
+      document.getElementById("surveyAnswer4").style.display = "none";
+      document.getElementById("surveyMaxCharacters2").style.display = "none"
+      document.getElementById("surveySubmit").style.display = "none"
+    },
+
     methods: {
+
+      // Vue 3 Method used for Survey Next Button
+      surveyValidateFirstStep() {
+        if (selectNextOptionBTN_counter === 1) {
+          document.getElementById("surveyQuestion1").style.display = "";
+          document.getElementById("surveyAnswer1").style.display = "";
+          document.getElementById("surveyQuestion2").style.display = "";
+          document.getElementById("surveyAnswer2").style.display = "";
+          document.getElementById("surveyMaxCharacters").style.display = "";
+          document.getElementById("surveyBackButton").style.display = "";
+          document.getElementById("surveyQuestion3").style.display = "none";
+          document.getElementById("surveyAnswer3").style.display = "none";
+          document.getElementById("surveyQuestion4").style.display = "none";
+          document.getElementById("surveyAnswer4").style.display = "none";
+          document.getElementById("surveyMaxCharacters2").style.display = "none";
+          document.getElementById("surveySubmit").style.display = "none";
+        }
+
+        if (selectNextOptionBTN_counter === 2) {
+          document.getElementById("surveyBackButton").style.display = "";
+          document.getElementById("surveyQuestion3").style.display = "";
+          document.getElementById("surveyAnswer3").style.display = "";
+          document.getElementById("surveyQuestion4").style.display = "";
+          document.getElementById("surveyAnswer4").style.display = "";
+          document.getElementById("surveyMaxCharacters2").style.display = "";
+          document.getElementById("surveyQuestion1").style.display = "none";
+          document.getElementById("surveyAnswer1").style.display = "none";
+          document.getElementById("surveyQuestion2").style.display = "none";
+          document.getElementById("surveyAnswer2").style.display = "none";
+          document.getElementById("surveyMaxCharacters").style.display = "none";
+          document.getElementById("surveySubmit").style.display = "";
+          document.getElementById("surveyNextOption").style.display = "none";
+        }
+        selectNextOptionBTN_counter = selectNextOptionBTN_counter + 1
+      },
+
+      // Vue 3 Method used for Survey Back Button
+      surveyBackStep(){
+        if (selectNextOptionBTN_counter === 2) {
+          document.getElementById("surveyBackButton").style.display = "none";
+          document.getElementById("surveyQuestion1").style.display = "none";
+          document.getElementById("surveyAnswer1").style.display = "none";
+          document.getElementById("surveyQuestion2").style.display = "none";
+          document.getElementById("surveyAnswer2").style.display = "none";
+          document.getElementById("surveyMaxCharacters").style.display = "none";
+          document.getElementById("surveyQuestion3").style.display = "none";
+          document.getElementById("surveyAnswer3").style.display = "none";
+          document.getElementById("surveyQuestion4").style.display = "none";
+          document.getElementById("surveyAnswer4").style.display = "none";
+          document.getElementById("surveyMaxCharacters2").style.display = "none";
+          document.getElementById("surveySubmit").style.display = "none";
+        }
+
+        if (selectNextOptionBTN_counter === 3) {
+          document.getElementById("surveyQuestion1").style.display = "";
+          document.getElementById("surveyAnswer1").style.display = "";
+          document.getElementById("surveyQuestion2").style.display = "";
+          document.getElementById("surveyAnswer2").style.display = "";
+          document.getElementById("surveyMaxCharacters").style.display = "";
+          document.getElementById("surveyBackButton").style.display = "";
+          document.getElementById("surveyQuestion3").style.display = "none";
+          document.getElementById("surveyAnswer3").style.display = "none";
+          document.getElementById("surveyQuestion4").style.display = "none";
+          document.getElementById("surveyAnswer4").style.display = "none";
+          document.getElementById("surveyMaxCharacters2").style.display = "none";
+          document.getElementById("surveySubmit").style.display = "none";
+          document.getElementById("surveyNextOption").style.display = "";
+        }
+        selectNextOptionBTN_counter = selectNextOptionBTN_counter - 1;
+      },
+
+
       close() {
         this.clearValues();
         this.$emit('close');
@@ -112,6 +203,7 @@
         this.$cookies.set(this.surveyCookie,"true");
       },
 
+
     },
   }
 </script>
@@ -127,41 +219,64 @@
         <section class="modal-body-survey" id="modalDescription">
           <slot name="body">
               <div>
-                <form-wizard @on-complete="onComplete"
-                        start-index:="0"
-                        step-size="xs"
-                        title="Report Export Survey"
-                        subtitle="Thank you for helping us improve your reporting experience."
-                        finish-button-text="Submit"
-                        color="gray"
-                        error-color="#a94442"
-                        ref="wizard"
-                     >
-                     <tab-content title="Overview"
-                                  icon="ti-info-alt">
+                <div class="vue-form-wizard">
+                  <div class="wizard-header">
+                    <h4 class="wizard-title">Report Export Survey</h4>
+                    <p class="category">Thank you for helping us improve your reporting experience.</p>
+                  </div>
+                </div>
+                <p id = "surveyMessage">
                       EVS is committed to providing you with an elegant and robust mechanism
                         to retrieve the data of interest to you.
-                       Please send feedback to NCIThesarus@nci.nih.gov
-                     </tab-content>
-                    <tab-content title="Features"
-                                 icon="ti-stats-up" :before-change="validateFirstTab">
-                       <vue-form-generator :model="survey"
-                                           :schema="firstTabSchema"
-                                           :options="formOptions"
-                                           ref="firstTabForm"
-                                           >
-                       </vue-form-generator>
-                    </tab-content>
-                    <tab-content title="Additional Info"
-                                 icon="ti-comment-alt" :before-change="validateSecondTab">
-                     <vue-form-generator :model="survey"
-                                           :schema="secondTabSchema"
-                                           :options="formOptions"
-                                           ref="secondTabForm"
-                                           >
-                       </vue-form-generator>
-                    </tab-content>
-                </form-wizard>
+                      Thank you for taking this brief survey.
+                </p>
+
+                <span id = "surveyQuestion1">
+                  How would you rate the ease of retrieving the desired data? 1 is difficult, 10 is easy
+                </span>
+
+                <select id="surveyAnswer1" class="form-control"><option disabled="disabled" value="">&lt;Nothing selected&gt;</option><!----><option value="1">1</option><!----><option value="2">2</option><!----><option value="3">3</option><!----><option value="4">4</option><!----><option value="5">5</option><!----><option value="6">6</option><!----><option value="7">7</option><!----><option value="8">8</option><!----><option value="9">9</option><!----><option value="10">10</option></select>
+                <br>
+                <span id = "surveyQuestion2">
+                  What features would you like to see on this application?
+                </span>
+
+                <textarea id="surveyAnswer2" maxlength="500" placeholder="suggest features" rows="3" class="form-control"></textarea>
+
+                <div class="hint" id = "surveyMaxCharacters">Max 500 characters</div>
+                <br>
+
+
+                <span id = "surveyQuestion3">
+                  What features would you like to see on this application?
+                </span>
+
+                <textarea id="surveyAnswer3" maxlength="500" placeholder="suggest features" rows="3" class="form-control"></textarea>
+
+                <div class="hint" id = "surveyMaxCharacters2">Max 500 characters</div>
+
+                <br>
+
+
+                <span id = "surveyQuestion4">
+                  Please supply your email if you would like a personal response.
+                </span>
+
+                <input id="surveyAnswer4" type = "text" class="form-control">
+
+                <br>
+                <span role="button" tabindex="0">
+                  <center>
+                    <button tabindex="-1" type="button" id = "surveyBackButton" class="Survey-btn" v-on:click="surveyBackStep()"  style="background-color: rgb(1, 126, 190); border-color: rgb(1, 126, 190); color: white;"> Back </button>
+
+                    <button tabindex="-1" type="button" id = "surveyNextOption" class="Survey-btn" v-on:click="surveyValidateFirstStep()"  style="background-color: rgb(1, 126, 190); border-color: rgb(1, 126, 190); color: white;"> Next </button>
+
+                    <button tabindex="-1" type="button" id = "surveySubmit" class="Survey-btn" v-on:click="submitForm()"  style="background-color: rgb(1, 126, 190); border-color: rgb(1, 126, 190); color: white;"> Submit </button>
+
+                  </center>
+                </span>
+
+
               </div>
           </slot>
         </section>
@@ -233,5 +348,25 @@
     overflow-y: auto;
     position: relative;
     padding: 20px 10px;
+  }
+
+  .Survey-btn{
+    background-color: rgb(0, 125, 188);
+    border-color: rgb(0, 125, 188);
+    color: white;
+    padding: 5px;
+    border-radius: 4px;
+    margin-left: 2px;
+    width: 158px;
+  }
+
+  .Survey-btn1{
+    background-color: rgb(0, 125, 188);
+    border-color: rgb(0, 125, 188);
+    color: white;
+    padding: 5px;
+    border-radius: 4px;
+    margin-left: 600px;
+    width: 158px;
   }
 </style>
